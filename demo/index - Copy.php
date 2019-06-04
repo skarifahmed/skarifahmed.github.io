9c38f583a9fa934d5ae504b3ee6b1bdbaa771aec
skarifahmed <31127476+skarifahmed@users.noreply.github.com>
2019-06-04 18:37:55 +0200

    Add files via upload

---
+++ demo/index - Copy.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Video Synopsis</title>
+<style type="text/css">
+<!--
+.style1 {
+	font-size: x-large;
+	font-weight: bold;
+}
+.style2 {
+	color: #FF0000;
+	font-weight: bold;
+}
+.style3 {color: #FF0000}
+-->
+</style>
+</head>
+
+<body>
+<table width="100%" border="0">
+  <tr>
+    <td width="7%">&nbsp;</td>
+    <td width="84%"> <span class="style1">Query-based Video Synopsis using Trajectory Clustering </span><br /></td>
+    <td width="9%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><table width="100%" border="0">
+      <tr>
+        <td colspan="4" bgcolor="#33CCFF"><strong>Comparison [<span class="style3">Master Synopsis of 30 minutes video IIT-I dataset</span>] </strong></td>
+        </tr>
+      <tr>
+        <td width="31%">
+		<video width="320" height="240" controls preload="none">
+   <source src="video/proposed_seg1_x264.mp4#2" type="video/mp4">
+   Your browser does not support the video tag.</video>		</td>
+        <td width="34%">
+		<video width="320" height="240" controls preload="none">
+   <source src="video/output_0_x264.mp4#2" type="video/mp4">
+   Your browser does not support the video tag.</video></td>
+     
+        <td width="35%">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="31%">
+		  <span class="style2">Proposed		  </span></td>
+        <td width="34%">
+		  <span class="style2">Pritch et. al[] with collusion cost 0%		  </span></td>
+        <td width="35%"><span class="style2">Pritch et. al[] with collusion cost 10% </span></td>
+	   </tr>
+      <tr>
+	  
+	  
+          <td width="31%">
+	<video width="320" height="240" controls preload="none">
+   <source src="video/output_10_x264.mp4#2" type="video/mp4">
+   Your browser does not support the video tag.</video></td>
+ <td width="34%">
+<video width="320" height="240" controls preload="none">
+   <source src="video/output_15_x264.mp4#2" type="video/mp4">
+   Your browser does not support the video tag.</video></td>
+      <td width="35%">&nbsp;</td>
+      </tr>
+	   
+	   <tr>
+        <td width="31%">&nbsp;</td>
+        <td width="34%">
+		  <span class="style2">Pritch et. al[]	with collusion cost 15%	  </span></td>
+        <td width="35%">&nbsp;</td>
+	   </tr>
+    </table></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><table width="100%" border="0">
+      <tr>
+        <td>Query [IIT-I] 4 hours </td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><img src="img/iit_1_reg.jpg" width="132" height="113" /></td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ demo/play_video.php
@@ -0,0 +1,9 @@
+<html><body>
+<p><strong>Synopsis Video </strong></p>
+        <p>
+								<video width="320" height="240" controls autoplay>
+  <source src="video/<?php echo $_GET['id'];?>" type="video/mp4">
+  Your browser does not support the video tag.</video> 
+  
+</body>
+</html>
